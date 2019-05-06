16234f19f65c8641d47f8a80a28375c6208378c7
Bryna Dunnells <badunnells@gmail.com>
2019-05-06 23:46:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
       if value < lowest_value || lowest_value == nil
         lowest_key << key
       end
-    
+     return lowest_value
     end
 
      return lowest_key
