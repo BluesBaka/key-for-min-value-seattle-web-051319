fa5ce4599d41f895cbfc554f058eb8ace45e6d6b
Bryna Dunnells <badunnells@gmail.com>
2019-05-06 23:36:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
     lowest_value = nil
      name_hash.each do |key, value |
 
-      if value < lowest_key
-        lowest_key << key
+      if value < lowest_value
+        lowest_ << key
       end
       #  puts "#{key} is #{value}"
       #something to compare and return the lowest value
